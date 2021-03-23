04cb805c1d8c38ec0a4d3f276180289be5cf4f0f
AVor0n <AVor0n@yandex.ru>
2021-03-24 00:29:41 +0400

    feat: add toggle letter-note

---
+++ script.js
@@ -0,0 +1,14 @@
+const piano = document.querySelector(`.piano`);
+const pianoKeys = document.querySelectorAll(`.piano-key`);
+const btnContainer = document.querySelector(`.btn-container`);
+const buttons = document.querySelectorAll(`.btn`)
+
+//!--------------Toggle button handler-----------------------------------//
+
+btnContainer.addEventListener(`click`, (e) => {
+    if (e.target.classList.contains(`btn-active`)) {
+        return;
+    }
+    buttons.forEach(btn => btn.classList.toggle(`btn-active`));
+    pianoKeys.forEach(pianoKey => pianoKey.classList.toggle(`piano-key-letter`));
+})
